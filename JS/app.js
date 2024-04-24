68ef1747aae52503df19c79a963c51290f167166
Varun Mangla <iamvarunmangla@gmail.com>
2024-04-25 01:12:18 +0530

    added files

---
+++ JS/app.js
@@ -0,0 +1,24 @@
+const endDate = '01 July 2024 10:00 AM'
+document.getElementById('end-date').innerText = endDate;
+
+const inputs = document.querySelectorAll('input')
+function clock() {
+    const end = new Date(endDate)
+    const now = new Date()
+    const diff = (end - now) / 1000;
+
+    if (diff<0) return;
+    
+    inputs[0].value=Math.floor(diff / 3600 / 24);
+    inputs[1].value=Math.floor((diff /3600)%24);
+    inputs[2].value=Math.floor((diff /60)%60);
+    inputs[3].value=Math.floor((diff)%60);
+    
+
+}
+
+setInterval(
+    ()=>{
+    clock()
+    },1000
+)
